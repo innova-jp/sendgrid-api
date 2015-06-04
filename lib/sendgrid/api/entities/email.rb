aabd922c3c37135a9d6bbfb506ec532ceb86c839
Satoru Mikami <saicologic@gmail.com>
2015-06-04 16:21:58 +0900

    Support custom variables for Marketing Email

--- lib/sendgrid/api/entities/email.rb
+++ lib/sendgrid/api/entities/email.rb
@@ -7,6 +7,15 @@ module Sendgrid
 
         attribute :email, :name
 
+        def initialize(attributes = {})
+          custom_keys = attributes.keys - [:email, :name]
+          if custom_keys.size > 0
+            custom_keys.each{|custom_key|
+              self.class.class_eval { attribute custom_key }
+            }
+          end
+          super
+        end
       end
     end
   end
